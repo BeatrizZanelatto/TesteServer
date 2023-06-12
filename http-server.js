0266e2d50acc24d4ab25daf534288797a0964487
BeatrizZanelatto <104568663+BeatrizZanelatto@users.noreply.github.com>
2023-06-12 19:33:28 +0000

    testess:)

--- http-server.js
+++ http-server.js
@@ -7,22 +7,25 @@ app.use(express.static(__dirname + '/public'))
 app.use("/create", (req, res) => {
   const { file, text } = req.query
   fs.writeFileSync(file, text)
-  res.send(new Date()) // creado o arquivo tal tal tal
+  res.send(console.log ("arquivo criado!"))
 })
 
 app.use("/read", (req, res) => {
   const { file } = req.query
-  // fs.readFileSync
+  const text = fs.readFileSync(file)
+  res.send(text.toString())
 })
 
 app.use("/update", (req, res) => {
   const { file, text } = req.query
-  // fs.appendFileSync
+  fs.appendFileSync(file, text)
+  res.send(console.log ("arquivo atualizado!"))
 })
 
 app.use("/delete", (req, res) => {
   const { file } = req.query
-  // fs.rmSync
+  fs.rmSync(file)
+  res.send(console.log("arquivo excluído!"))
 })
 
 app.listen(3000, () => console.log("Servidor rodando!"))
